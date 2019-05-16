a0ebae10f068e43cd847209a5eb606d86f6bbc71
20160217012 <3526381910@.com>
2019-05-16 09:30:46 +0800

    提交到本地仓库

---
+++ Git/src/com/nnxy/studentModel.java
@@ -0,0 +1,16 @@
+package com.nnxy;
+
+public class studentModel {	
+	private int id;
+	private int sex;
+	private String username;
+	private String userpassword;
+	public studentModel(int id, int sex, String username, String userpassword) {
+		super();
+		this.id = id;
+		this.sex = sex;
+		this.username = username;
+		this.userpassword = userpassword;
+	}
+	
+}
